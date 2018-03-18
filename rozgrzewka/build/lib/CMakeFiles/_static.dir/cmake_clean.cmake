5c560b93c7b9a2087c7ab96a6c19f181cdbb1986
kriczq <kriczq@gmail.com>
2018-03-19 00:06:17 +0100

    segmentation success

--- rozgrzewka/build/lib/CMakeFiles/_static.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "lib_static.pdb"
-  "lib_static.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
